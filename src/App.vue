1250ea8d7c7f5a74e2249b04cd12a6c0f4ebeda3
眼镜 <1057427161@qq.com>
2020-01-02 21:00:17 +0800

    登录页面结构完成度1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="content">
     <router-view></router-view>
   </div>
 </template>
@@ -11,5 +11,7 @@ export default {
 </script>
 
 <style>
-
+.content {
+  height: 100%;
+}
 </style>
