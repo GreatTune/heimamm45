bb0d44237942f18c9fb61e5b4c756c0afb99fedc
眼镜 <1057427161@qq.com>
2020-01-06 21:13:04 +0800

    首页模块 - 嵌套路由整合

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,14 @@ Vue.use(VueRouter)
 import login from '../views/login/login.vue'
 import index from '../views/index/index.vue'
 
+// 嵌套路由组件
+import chart from "../views/index/chart/chart.vue"
+import user from "../views/index/user/user.vue" 
+import question from "../views/index/question/question.vue" 
+import enterprise from "../views/index/enterprise/enterprise.vue" 
+import subject from "../views/index/subject/subject.vue" 
+
+
 const router = new VueRouter({ // 实例化
   routes: [
     {
@@ -15,7 +23,29 @@ const router = new VueRouter({ // 实例化
     },
     {
       path: "/index",
-      component: index
+      component: index,
+      children: [
+        {
+          path: "chart",
+          component: chart 
+        },
+        {
+          path: "user",
+          component: user
+        },
+        {
+          path: "question",
+          component: question
+        },
+        {
+          path: "enterprise",
+          component: enterprise
+        },
+        {
+          path: "subject",
+          component: subject
+        },
+      ]
     }
   ]
 })
