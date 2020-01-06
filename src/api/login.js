177c55333cec773cb5d384b2d726ea5aaf2fe3be
眼镜 <1057427161@qq.com>
2020-01-06 17:27:57 +0800

    首页模块 - 顶部布局

--- src/api/login.js
+++ src/api/login.js
@@ -22,6 +22,24 @@ export function sendsms(data){
 export function register(data){
   return axios({
     url: process.env.VUE_APP_BASEURL + "/register",
+    method: "get", // 请求类型
+    withCredentials: true, // 跨域 , 是否携带
+    data// 参数
+  })
+}
+// 定义获取用户方法  并暴露出去
+export function info(data){
+  return axios({
+    url: process.env.VUE_APP_BASEURL + "/info",
+    method: "post", // 请求类型
+    withCredentials: true, // 跨域 , 是否携带
+    data// 参数
+  })
+}
+// 定义退出登录方法  并暴露出去
+export function logout(data){
+  return axios({
+    url: process.env.VUE_APP_BASEURL + "/logout",
     method: "post", // 请求类型
     withCredentials: true, // 跨域 , 是否携带
     data// 参数
---
+++ src/views/index/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <el-container>
+    <el-header>
+      <!-- 左侧 -->
+      <div class="header-left">
+        <i class="left-icon el-icon-s-fold"></i>
+        <img class="left-img" src="../../assets/logo (2).png" alt />
+        <span class="left-text">黑马面面</span>
+      </div>
+      <!-- 右侧 -->
+      <div class="header-right">
+        <img src="../../assets/02.jpg" alt />
+        <span class="right-text">疑惑猫,您好</span>
+        <el-button @click="getlogout" type="primary" size="small">退出</el-button>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    // 退出按钮点击事件
+    getlogout(){
+      
+    }
+  },
+};
+</script>
+
+<style lang="less">
+.el-container {
+  height: 100%;
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    background: rgba(255, 255, 255, 1);
+    box-shadow: 0px 2px 5px 0px rgba(63, 63, 63, 0.35);
+    line-height: 60px;
+    // 顶部左边属性设置
+    .header-left {
+      display: flex;
+      align-items: center;
+      .left-icon {
+        font-size: 24px;
+      }
+      .left-img {
+        margin-left: 22px;
+        margin-right: 11px;
+      }
+      .left-text {
+        font-size: 22px;
+        color: #49a1ff;
+      }
+    }
+    // 顶部右侧属性设置
+    .header-right {
+      display: flex;
+      align-items: center;
+      img {
+        width: 43px;
+        height: 43px;
+        border-radius: 50%;
+      }
+      .right-text {
+        margin-left: 9px;
+        margin-right: 38px
+      }
+    }
+  }
+}
+</style>
